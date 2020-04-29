f4c9bf8fd5590e080735ddb6a2523d435bcfb2a0
yubingqian <55077749+yubingqiangit@users.noreply.github.com>
2020-04-29 18:47:38 +0800

    1修改

--- src/enumTest/preauthDemo/HelloWorld.java
+++ src/enumTest/preauthDemo/HelloWorld.java
@@ -3,5 +3,6 @@ package enumTest.preauthDemo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("holle world!!!");
+        System.out.println("22222222222222222");
     }
 }
