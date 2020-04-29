ca4bc1514d79c7285661ad9a2b0413806e159000
yubingqian <55077749+yubingqiangit@users.noreply.github.com>
2020-04-29 19:31:40 +0800

    s

--- src/enumTest/preauthDemo/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package enumTest.preauthDemo;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("holle world!!!");
-        System.out.println("22222222222222222");
-    }
-}
