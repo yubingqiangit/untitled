70adfc8ab4730fbc8839d63c087daa85d560b519
慢羊羊突破 <55077749+yubingqiangit@users.noreply.github.com>
2020-04-29 18:28:43 +0800

    Update and rename src/enumTest/preauthDemo/StrategyTest.java to  src/ enumTest/ preauthDemo/StrategyTest.java

--- src/ enumTest/ preauthDemo/StrategyTest.java
+++ src/ enumTest/ preauthDemo/StrategyTest.java
@@ -9,7 +9,7 @@ public class StrategyTest {
         System.out.println("鉴权结果1" + b);
         System.out.println("鉴权结果2" + c);
         System.out.println("鉴权结果3" + d);
-        aaaab
+        affffffffafffff
         */
 
         Context context = new Context(new GWstake());
