d81851bafbad64ab6b9e337356bc7dab2760289c
yubingqian <55077749+yubingqiangit@users.noreply.github.com>
2020-04-29 19:04:37 +0800

    3333

--- src/enumTest/preauthDemo/HAHAH.java
+++ src/enumTest/preauthDemo/HAHAH.java
@@ -3,5 +3,10 @@ package enumTest.preauthDemo;
 public class HAHAH {
     public static void main(String[] args) {
         System.out.println("fdfdf");
+        System.out.println("33333333");
+        System.out.println("33333333");
+        System.out.println("33333333");
+        System.out.println("33333333");
+        System.out.println("33333333");
     }
 }
