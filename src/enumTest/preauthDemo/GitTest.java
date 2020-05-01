4551392c060361fb621bcb76554a5e5e0bdd88a4
yubingqian <55077749+yubingqiangit@users.noreply.github.com>
2020-05-01 23:18:48 +0800

    master合并后新增次提交

--- src/enumTest/preauthDemo/GitTest.java
+++ src/enumTest/preauthDemo/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("我是dev第二次提交");
          System.out.println("我是dev第三次提交");
         System.out.println("我是devpolye第二次提交");
+        System.out.println("我是master第二次提交");
     }
 }
 
