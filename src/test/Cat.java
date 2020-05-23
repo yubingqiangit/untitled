e357c21251d98e8020539f703f4f0caa8fd028d4
yubingqian <55077749+yubingqiangit@users.noreply.github.com>
2020-05-23 09:24:07 +0800

    初始化提交

---
+++ src/test/Cat.java
@@ -0,0 +1,27 @@
+package test;
+
+import java.util.ArrayList;
+
+public class Cat {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        String one = getOne(list);
+        System.out.println(one);
+
+    }
+
+
+    /**
+     * 获取集合第一个参数
+     * @param list
+     * @param <T>
+     * @return
+     */
+    public static <T>T getOne(ArrayList<T> list){
+
+        return list.get(0);
+    }
+}
