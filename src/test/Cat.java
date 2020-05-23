d80f9076e970fc4ac61b2bd5ae83ae14e078f10c
yubingqian <55077749+yubingqiangit@users.noreply.github.com>
2020-05-23 13:54:33 +0800

    344

--- src/test/Cat.java
+++ src/test/Cat.java
@@ -6,6 +6,15 @@ public class Cat {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
         list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
         list.add("2");
         list.add("3");
         String one = getOne(list);
