6b8256e59227510d70171ac3f299db49c28528c7
慢羊羊突破 <55077749+yubingqiangit@users.noreply.github.com>
2020-05-23 14:00:12 +0800

    Update Cat.java

--- src/test/Cat.java
+++ src/test/Cat.java
@@ -6,20 +6,9 @@ public class Cat {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
         list.add("1");
-        list.add("1");
-        list.add("1");
-        list.add("1");
-        list.add("1");
-        list.add("1");
-        list.add("1");
-        list.add("1");
-        list.add("1");
-        list.add("1");
-        list.add("2");
-        list.add("3");
+      
         String one = getOne(list);
-              String one = getOne(list);
-              String one = getOne(list);
+            
 
     }
 
